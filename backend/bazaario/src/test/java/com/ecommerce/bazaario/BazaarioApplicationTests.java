d9b718ab7c0d1418d96cbc83a6b92ffe14975a9a
Chahel <chahel1817@gmail.com>
2026-06-15 20:52:24 +0530

    Initial Commit

---
+++ backend/bazaario/src/test/java/com/ecommerce/bazaario/BazaarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bazaario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
